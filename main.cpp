2afd7e887c1fe1a4fc33fd2c9b140753ad70a289
JuliaAlanne <53877127+JuliaAlanne@users.noreply.github.com>
2022-05-28 11:07:51 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,5 +9,6 @@ int main()
     objeto3d.setColor(0.5,0.0,0.0,1.0);
     objeto3d.cutBox(55,65,32,42,58,68);
     objeto3d.writeOFF((const char*) "C:/Users/julia/projeto/.OFF.off");
+    std::cout << "ta rodando gata bjs";
     return 0;
 }
